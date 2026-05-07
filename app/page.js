3b34aced7a80c5e2354a4904ff9b896ce54e1dde
profoundroomkh <profoundroom.kh@gmail.com>
2026-05-08 05:14:44 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <main>
+    <main style={{
+      backgroundColor: "black",
+      color: "white",
+      minHeight: "100vh",
+      padding: "40px"
+    }}>
       <h1>Profound Room</h1>
+      <p>Luxury Relaxation Space</p>
     </main>
   )
 }
