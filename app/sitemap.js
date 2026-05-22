9cba31073ac1667c00b3533d35e96da3852a1a6f
profoundroomkh <profoundroom.kh@gmail.com>
2026-05-22 19:46:19 +0800

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,1 +1,29 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://profound-room.vercel.app',
+      lastModified: new Date(),
+    },
 
+    {
+      url:
+        'https://profound-room.vercel.app/space',
+
+      lastModified: new Date(),
+    },
+
+    {
+      url:
+        'https://profound-room.vercel.app/reservation',
+
+      lastModified: new Date(),
+    },
+
+    {
+      url:
+        'https://profound-room.vercel.app/journal',
+
+      lastModified: new Date(),
+    },
+  ]
+}
