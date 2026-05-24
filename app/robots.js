ae2fdcd84209646b6be8f2ea619d548cd8da255b
profoundroomkh <profoundroom.kh@gmail.com>
2026-05-24 11:35:13 +0800

    Update robots.js

--- app/robots.js
+++ app/robots.js
@@ -1,1 +1,11 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
 
+    sitemap:
+      'https://profound-room.vercel.app/sitemap.xml',
+  }
+}
