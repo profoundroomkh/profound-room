ec94444aa4e9b4da4106dcc0f97f1f307dfab741
profoundroomkh <profoundroom.kh@gmail.com>
2026-05-25 22:49:43 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 'use client'
 
+import { useState, useEffect } from 'react'
+
 import Header from '../components/Header'
 import FAQ from '../components/FAQ'
 import Gallery from '../components/Gallery'
