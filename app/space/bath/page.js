dc7aece8031cbaff38dc372768be0765c5d6a4e3
profoundroomkh <profoundroom.kh@gmail.com>
2026-05-25 19:20:22 +0800

    Update page.js

--- app/space/bath/page.js
+++ app/space/bath/page.js
@@ -1,1 +1,20 @@
+import Header from '../../../components/Header'
 
+export default function BathPage() {
+  return (
+    <>
+      <Header />
+
+      <main
+        style={{
+          background: '#050505',
+          color: '#f2e1d0',
+          minHeight: '100vh',
+          padding: '140px 24px',
+        }}
+      >
+        <h1>Bath Space</h1>
+      </main>
+    </>
+  )
+}
