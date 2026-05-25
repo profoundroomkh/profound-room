62141feb34d36c4b9619491dadefa3a262f2501b
profoundroomkh <profoundroom.kh@gmail.com>
2026-05-25 19:20:37 +0800

    Update page.js

--- app/space/detail/page.js
+++ app/space/detail/page.js
@@ -1,1 +1,20 @@
+import Header from '../../../components/Header'
 
+export default function DetailPage() {
+  return (
+    <>
+      <Header />
+
+      <main
+        style={{
+          background: '#050505',
+          color: '#f2e1d0',
+          minHeight: '100vh',
+          padding: '140px 24px',
+        }}
+      >
+        <h1>Space Detail</h1>
+      </main>
+    </>
+  )
+}
