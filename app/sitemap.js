12bf85ebda8e23d3e83ef1add5b406bfd86b42f3
profoundroomkh <profoundroom.kh@gmail.com>
2026-06-14 10:37:52 +0800

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,27 +1,84 @@
 export default function sitemap() {
+  const baseUrl = 'https://profoundroom.com'
+
   return [
     {
-      url: 'https://profound-room.vercel.app',
+      url: `${baseUrl}`,
       lastModified: new Date(),
     },
 
     {
-      url: 'https://profound-room.vercel.app/space',
+      url: `${baseUrl}/space`,
       lastModified: new Date(),
     },
 
     {
-      url: 'https://profound-room.vercel.app/reservation',
+      url: `${baseUrl}/reservation`,
       lastModified: new Date(),
     },
 
     {
-      url: 'https://profound-room.vercel.app/journal',
+      url: `${baseUrl}/journal`,
       lastModified: new Date(),
     },
 
     {
-      url: 'https://profound-room.vercel.app/faq',
+      url: `${baseUrl}/faq`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/journal/first-gay-spa-experience`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/journal/immersive-relaxation-experience`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/journal/kaohsiung-gay-spa`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/journal/kaohsiung-mens-massage`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/journal/kaohsiung-night-relax-space`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/journal/kaohsiung-private-spa`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/journal/mens-spa-guide-kaohsiung`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/journal/mens-spa-vs-normal-massage`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/journal/private-spa-for-men`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/journal/private-spa-safety`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/journal/why-people-need-relaxation-at-night`,
       lastModified: new Date(),
     },
   ]
