2a51951bb7299f131eabef6c186f4cc8bc9ad77d
profoundroomkh <profoundroom.kh@gmail.com>
2026-07-09 13:19:12 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,6 @@ import { useState, useEffect } from 'react'
 import Header from '../components/Header'
 import FAQ from '../components/FAQ'
 import Gallery from '../components/Gallery'
-  export default function Home() {
 const [currentIndexes, setCurrentIndexes] =
   useState([0, 0, 0, 0, 0, 0, 0, 0])
 useEffect(() => {
