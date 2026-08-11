b27a667d6d7c1a1ac005a28798a99abe54ddf620
profoundroomkh <profoundroom.kh@gmail.com>
2026-08-11 15:44:26 +0800

    Update page.js

--- app/journal/private-spa-safety/page.js
+++ app/journal/private-spa-safety/page.js
@@ -1,4 +1,5 @@
 import Header from '../../../components/Header'
+import JournalCTA from '../../../components/JournalCTA'
 
 export default function PrivateSpaSafety() {
   return (
@@ -42,7 +43,7 @@ export default function PrivateSpaSafety() {
             <br />
             真的比較安全嗎？
           </h1>
-
+  <JournalCTA />
           <p
             style={{
               color: 'rgba(255,255,255,0.68)',
