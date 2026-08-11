7ad0fc4c93f81a2240b10507cf7df707c351ec37
profoundroomkh <profoundroom.kh@gmail.com>
2026-08-11 15:42:30 +0800

    Update page.js

--- app/journal/mens-spa-vs-normal-massage/page.js
+++ app/journal/mens-spa-vs-normal-massage/page.js
@@ -1,4 +1,5 @@
 import Header from '../../../components/Header'
+import JournalCTA from '../../../components/JournalCTA'
 
 export default function MensSpaVsMassagePage() {
   return (
@@ -42,7 +43,7 @@ export default function MensSpaVsMassagePage() {
             <br />
             最大的差別是什麼？
           </h1>
-
+  <JournalCTA />
           <p
             style={{
               color: 'rgba(255,255,255,0.68)',
